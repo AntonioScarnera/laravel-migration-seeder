b1e30131eecdcfba01b46be0c84b6a3063948458
AntonioScarnera <scarnerantonio07@gmail.com>
2022-06-14 13:56:34 +0200

    base layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layout.default')
+
+
+@section('pageTitle', 'Home Page')
+
+@section('mainContent')
+
+    <h1 class="text-center">
+        Home 
+        <i class="fa-solid fa-home"></i>
+    </h1>
+    
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+   <h5>Made with love by #59</h5> 
+</footer>
